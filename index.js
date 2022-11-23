47b47da9b5e85f3093c7f5e9b72c537455098863
Sirumba <simonsmaina45@gmail.com>
2022-11-23 14:50:02 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // Code your solution in this file!
+
+let companyName = "Scuber"
+const companyName1 = "Scuber"
+
+let mostProfitableNeighborhood = "Chelsea"
+
+let companyCeo = "Susan Smith"
+
+
+
+
+
